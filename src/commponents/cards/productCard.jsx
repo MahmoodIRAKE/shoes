5cdb4916c75b3fd67e46ae439444f4887fb6809c
MahmoodIRAKE <mhmod.eraqi@gmail.com>
2021-12-26 04:05:21 +0200

    last commit

--- src/commponents/cards/productCard.jsx
+++ src/commponents/cards/productCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Link } from "react-router-dom";
 import { removeData } from "../../api/firebaseApi";
@@ -20,7 +21,7 @@ const ProductCard = ({ obj}) => {
                 </div>
                 <div className="extra content">
                     <div className="ui two buttons">
-                    <Link to={`/product/${obj.id}`} className="item ui basic green button">
+                    <Link to={`/product/:${obj.id}`} className="item ui basic green button">
                        Update
                      </Link>
                         
